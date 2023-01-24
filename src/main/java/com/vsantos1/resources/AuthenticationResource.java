736e79559f7b64215093cf22407fc3422ba3b354
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-23 21:57:20 -0300

    feat: create user endpoint

--- src/main/java/com/vsantos1/resources/AuthenticationResource.java
+++ src/main/java/com/vsantos1/resources/AuthenticationResource.java
@@ -1,9 +1,29 @@
 package com.vsantos1.resources;
 
+import com.vsantos1.dtos.RegisterDTO;
+import com.vsantos1.services.AuthenticationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthenticationResource {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationResource(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterDTO registerDTO) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(authenticationService.register(registerDTO));
+    }
 }
--- src/main/java/com/vsantos1/resources/TestResource.java
+++ src/main/java/com/vsantos1/resources/TestResource.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/v1")
+@RequestMapping(value = "/api/v1")
 public class TestResource {
 
     @GetMapping(value = "/test")
