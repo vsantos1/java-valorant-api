6eafb7b891de68a10e227625a10c3f0a49bd3793
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-26 23:48:17 -0300

    feat: cloud file upload

---
+++ src/main/java/com/vsantos1/resources/FileResource.java
@@ -0,0 +1,28 @@
+package com.vsantos1.resources;
+
+import com.vsantos1.services.UploadFileService;
+import org.cloudinary.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping(value = "/api/v1")
+public class FileResource {
+
+    private final UploadFileService uploadFileService;
+
+    public FileResource(UploadFileService uploadFileService) {
+        this.uploadFileService = uploadFileService;
+    }
+
+
+    @PostMapping(value = "/upload/file/{folderName}/maps", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadPhoto(@PathVariable("folderName") String folderName, @RequestParam("file") MultipartFile file) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(uploadFileService.upload(file,folderName));
+
+    }
+}
---
+++ src/main/java/com/vsantos1/services/UploadFileService.java
@@ -0,0 +1,65 @@
+package com.vsantos1.services;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class UploadFileService {
+
+    @Value("${file.upload.api-key}")
+    private String API_KEY;
+
+    @Value("${file.upload.cloud-name}")
+    private String CLOUD_NAME;
+    @Value("${file.upload.api-secret}")
+    private String API_SECRET;
+
+
+    public UploadFileService() {
+    }
+
+    public Cloudinary getCloudinary() {
+        HashMap<String, String> config = new HashMap<>();
+        config.put("cloud_name", CLOUD_NAME);
+        config.put("api_key", API_KEY);
+        config.put("api_secret", API_SECRET);
+        return new Cloudinary(config);
+    }
+
+    public String upload(MultipartFile file, String folderName) {
+        try {
+            File uploadedFile = convertMultiPartToFile(file);
+            Map params = ObjectUtils.asMap(
+                    "public_id", folderName + "/" + UUID.randomUUID(),
+                    "overwrite", false,
+                    "resource_type", "auto",
+                    "transformation", new Transformation().width(500).height(500).crop("fill")
+            );
+            Map uploadResult = getCloudinary().uploader().upload(uploadedFile, params);
+            return uploadResult.get("url").toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
+    }
+}
